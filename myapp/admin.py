5721fd861d2c2ebd4637d0f156e25e37c06bed62
jataz <africajataz@gmail.com>
2024-06-30 22:36:08 +0200

    First Commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Permission
 from myapp.models import Quotation
 
 # Register your models here.
 admin.site.register(Quotation)
+admin.site.register(Permission)
